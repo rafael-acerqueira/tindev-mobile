d5f7cd9c3a4f3f443d5b60784d559bde62d72f81
Rafael <rafael.acerqueirah@gmail.com>
2019-08-11 10:31:28 -0300

    Store user id and navigate to Main Screen

--- App.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import Login from './src/screens/Login'
-
-const App = () => {
-	return <Login />
-}
-
-export default App
--- src/screens/Login/index.js
+++ src/screens/Login/index.js
@@ -1,22 +1,49 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { Image, Platform } from 'react-native'
 import logo from '../../assets/logo.png'
-
+import api from '../../services/api'
+import AsyncStorage from '@react-native-community/async-storage'
 import { Container, Input, Button, TextButton } from './styles'
 
-const Login = () => (
-	<Container behavior="padding" enabled={Platform.OS === 'ios'}>
-		<Image source={logo} />
-		<Input
-			autoCapitalize="none"
-			autoCorrect={false}
-			placeholder="Digite seu usuário no Github"
-			placeholderTextColor="#999"
-		/>
-		<Button>
-			<TextButton>Enviar</TextButton>
-		</Button>
-	</Container>
-)
+const Login = ({ navigation }) => {
+	const [username, setUsername] = useState('')
+
+	useEffect(() => {
+		const getUsers = () => {
+			const user = AsyncStorage.getItem('user')
+			if (user) {
+				navigation.navigate('Main', { user })
+			}
+		}
+		getUsers()
+	}, [])
+
+	const handleLogin = async () => {
+		const { data } = await api.post('devs', { username })
+
+		const { _id } = data
+
+		await AsyncStorage.setItem('user', _id)
+
+		navigation.navigate('Main', { _id })
+	}
+
+	return (
+		<Container behavior="padding" enabled={Platform.OS === 'ios'}>
+			<Image source={logo} />
+			<Input
+				autoCapitalize="none"
+				autoCorrect={false}
+				placeholder="Digite seu usuário no Github"
+				placeholderTextColor="#999"
+				value={username}
+				onChangeText={setUsername}
+			/>
+			<Button onPress={handleLogin}>
+				<TextButton>Enviar</TextButton>
+			</Button>
+		</Container>
+	)
+}
 
 export default Login
